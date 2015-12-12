7d7d0e5d54dab584d7124e18ca735aa98161eef1
gautierenaud <gautierenaud@gmail.com>
2015-12-12 16:56:52 +0100

    update the website

--- BaseDonnees/xQuerySamples/EnseignantUneMatiere.xq
+++ BaseDonnees/xQuerySamples/EnseignantUneMatiere.xq
@@ -3,15 +3,15 @@ xquery version "1.0";
 let $enseignants := doc("../BD/Enseignant.xml")
 let $matieres := doc("../BD/Matiere.xml")
 return
-
+<ans>
+{
   for $matiere in $matieres//Matiere,
       $enseignant in $enseignants//Enseignant,
       $matParEnseignant in $enseignant/MatID 
   where $matiere/@ID = $matParEnseignant/@MatiereID 
   return
-  <ans>
-    {$matiere/Nom}
-    {$enseignant/Personne}
-   
-    </ans>
+    $matiere/Nom
+    $enseignant/Personne
+}   
+</ans>
 
---
+++ Site/js/variables.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gautierenaud on 12/12/15.
+ */
+// liste des options disponibles dans le menu pour Enseignants
+var enseignantSideOptions = ["Matières Enseignées", "Par Matière", "Par Groupe", "Par Année", "Par Cursus"];
+
+// liste des options disponibles dans le menu pour les étudiants
+var etudiantSideOptions = ["Matières Suivies", "Par Matière", "Par Groupe", "Par Année", "Par Cursus", "Par Dettes"];
+
+// liste des options pour les matières
+var matiereSideOptions = ["Par UF"];
