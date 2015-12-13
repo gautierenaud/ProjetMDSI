abf8da7a5f7b1e4210181047fc90b82c6bf569a6
gautierenaud <gautierenaud@gmail.com>
2015-12-13 11:21:52 +0100

    Site + xquery pour dettes

---
+++ BaseDonnees/xQuerySamples/DetteEtudiant.xq
@@ -0,0 +1,30 @@
+let $etudiants := doc("../BD/Etudiant.xml")
+let $matieres  := doc("../BD/Matiere.xml")
+
+return
+<ans>
+{
+  for $etud in $etudiants//Etudiant
+  return
+    <Etudiant>
+    {$etud/Personne/Nom}
+    {$etud/Personne/Prenom}
+    {
+     for $matAnnee in $etud//MatieresParAnnee
+     return
+     <Annee valeur="{$matAnnee/@Annee}">
+     {
+          for $matEtud in $matAnnee/MatID,
+              $mat in $matieres//Matiere
+          where $matEtud/@MatiereID = $mat/@ID and avg($matEtud/Note/text()) < 10
+          return
+             <Matiere>
+               <Nom>{$mat/Nom/text()}</Nom>
+               <Moyenne>{avg($matEtud/Note/text())}</Moyenne>
+             </Matiere>
+      }
+      </Annee>
+    }
+    </Etudiant>
+}
+</ans>
---
+++ Site/js/main.js
@@ -0,0 +1,52 @@
+var actualActiveNavbar = "Enseignants";
+function changeActiveNavbar(id){
+    if (id != actualActiveNavbar){
+        $("#"+actualActiveNavbar).removeClass("active");
+        $("#"+id).addClass("active");
+        actualActiveNavbar = id;
+        loadSideBar(actualActiveNavbar);
+    }
+}
+
+// function to toggle the menu when "Universite" is clicked
+function toggleMenu(){
+    $("#wrapper").toggleClass("toggled");
+    $("#navIcon").toggleClass("glyphicon-menu-left").toggleClass("glyphicon-menu-hamburger");
+}
+
+// close the side bar if opened
+function closeSideMenu(){
+    if ($("#wrapper").attr("class") != "toggled")
+        toggleMenu();
+}
+
+var sideBar = $(".sidebar-nav");
+function loadSideBar(id){
+    sideBar.empty();
+    var tmpList;
+    switch (id){
+        case "Enseignants":
+            tmpList = enseignantSideOptions;
+            break;
+        case "Etudiants":
+            tmpList = etudiantSideOptions;
+            break;
+        case "Matieres":
+            tmpList = matiereSideOptions;
+            break;
+        default:
+            alert("Wrong ID: " + id);
+            break;
+    }
+
+    // title of the Side Bar
+    sideBar.append("<li class='sidebar-brand'><a>" + id + "</a></li>");
+    // load the content of the list
+    for (var i in tmpList){
+        sideBar.append("<li><a href='#'>" + tmpList[i] + "</a></li>");
+    }
+}
+
+function initContent(){
+    loadSideBar(actualActiveNavbar);
+}
